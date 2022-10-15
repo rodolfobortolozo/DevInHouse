fa1bc201d9049fbf87bf7f09147fa006ee615d69
Rodolfo Bortolozo <rodolfobortolozo@yahoo.com.br>
2022-10-15 17:07:50 -0300

    S02EX09

---
+++ M01S02/Exercicios/Ex09/ex09.js
@@ -0,0 +1,19 @@
+var valorInicial = 0;
+var valorRaiz = 0;
+var ProgAritmetica = 0;
+var lista = [];
+
+valorInicial = parseInt(prompt("Informe o valor Inicial"));
+valorRaiz = parseInt(prompt("Informe o valor Inicial"));
+
+ProgAritmetica = valorInicial;
+
+for(var i=0;i<10;i++){
+
+    console.log(ProgAritmetica);
+    lista.push(ProgAritmetica);
+    ProgAritmetica += valorRaiz
+
+}
+
+alert(`Os 10 primeiros elementos da PA são: ${lista}`); 
