1a1a6f4f562b6ac03fb413d3b7f2177f5799b80d
Rodolfo Bortolozo <rodolfobortolozo@yahoo.com.br>
2022-10-20 22:15:09 -0300

    Correção

--- M01S03/Exercicios/Ex04/app.js
+++ M01S03/Exercicios/Ex04/app.js
@@ -1,3 +1,11 @@
+/*
+
+Para exercitarmos spread com objetos:
+Em um arquivo app.js incluído em index.html escreva uma função de nome mesclaObjetos, 
+que aceita dois objetos como parâmetro e retorna um novo objeto que contem todos os pares chave-valor do primeiro e do segundo objetos.
+
+*/
+
 const objUm = { a: 1, b: 2 };
 const objDois = { c: 3, d: 4 };
 
