09a112b7c007603f7daf460b798ec4bf64954183
Rodolfo Bortolozo <rodolfobortolozo@yahoo.com.br>
2022-10-20 22:18:51 -0300

    Correção

--- M01S03/Exercicios/Ex05/app.js
+++ M01S03/Exercicios/Ex05/app.js
@@ -1,7 +1,20 @@
-const valores = [1, 2, 3, 4];
+/*
+
+Para treinar os conceitos de Rest.
+
+Crie dois arquivos:
+- index.html
+- app.js
+
+No arquivo apps.js, utilizando o operador Rest, crie uma função que receba como parâmetro uma quantidade indefinida de variáveis inteiras (números inteiros).
+
+A função deve retornar a soma de todos os parâmetros (...).
+Após executada a função, o resultado deve ser exibido no console.
+
+*/
 
 
-function somaTudo(valores) {
+function somaTudo(...valores) {
   let soma = 0;
 
   valores.forEach(valor => soma += valor );
@@ -10,6 +23,6 @@ function somaTudo(valores) {
 }
 
 
-const resultado = somaTudo(valores);
+const resultado = somaTudo(1,2,3,4,5,6,7,8,9);
 console.log(resultado);
 // resultado deve ser 10
