c28dd8909b88b95dcbbb41be0ab93ab181f1bc6b
Rodolfo Bortolozo <rodolfobortolozo@yahoo.com.br>
2022-10-20 21:12:27 -0300

    Estudos e Correções Exercicios

--- M01S03/Exercicios/Ex04/app.js
+++ M01S03/Exercicios/Ex04/app.js
@@ -1,7 +1,13 @@
 const objUm = { a: 1, b: 2 };
 const objDois = { c: 3, d: 4 };
 
-const mesclaObjetos = (array1, array2) => Object.assign({},array1,array2);
+
+//Spread
+const mesclaObjetos = (array1, array2) =>  { 
+        
+    return  {...array1, ...array2};
+
+};
 
 const novoObjeto = mesclaObjetos(objUm, objDois);
 
