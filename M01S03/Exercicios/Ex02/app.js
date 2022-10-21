73783548f89aa0ead0d12fa8fb11c802dff9ba1c
Rodolfo Bortolozo <rodolfobortolozo@yahoo.com.br>
2022-10-20 22:09:05 -0300

    Ajustes do Exercicio

--- M01S03/Exercicios/Ex02/app.js
+++ M01S03/Exercicios/Ex02/app.js
@@ -1,4 +1,16 @@
+/*
 
+Em um arquivo app.js (incluído em index.html) faça o seguinte:
+
+No documento js faça uma arrow function para retornar uma mensagem (string) de "Olá, Mundo!".
+
+Armazene esta arrow function em uma constante (const) de nome mensagemOla.
+
+Chame a função mensagemOla e exiba o retorno desta função no console.
+
+Ajuste a função mensagemOla para que receba um parâmetro nome e retorne a mensagem "Olá, [nome]!".
+
+*/
 const mensagemOla = nome => `Olá, ${nome}`;
 
 console.log(mensagemOla('Rodolfo'));
