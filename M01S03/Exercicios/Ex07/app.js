21d7ff6c6e7477474bed2d9690e8493920890c70
Rodolfo Bortolozo <rodolfobortolozo@yahoo.com.br>
2022-10-20 22:28:05 -0300

    Exercicio 07

--- M01S03/Exercicios/Ex07/app.js
+++ M01S03/Exercicios/Ex07/app.js
@@ -1,3 +1,23 @@
+/*Em um arquivo app.js incluído em index.html, crie um objeto com a seguinte estrutura:
+
+const pessoa = {
+  nome: 'Ada',
+  idade: 36
+  profissao: 'matemática'
+};
+Faça uma função que receba este objeto por parâmetro, desestruture-o no começo da função (utilizando destruct) criando três variáveis (nome, idade, profissao).
+
+Faça ainda com que a função retorne uma string com a seguinte mensagem:
+
+"Esta é Ada, tem 36 anos e é matemática."
+Concatene as variáveis criadas com destruct para formar a mensagem.
+
+Chame a função passando o objeto por parâmetro e exiba o retorno no console.
+
+Extra: Faça o destruct acontecer diretamente na linha de declaração dos parâmetros da função. Por fim faça com uma arrow function.
+
+*/
+
 const pessoa = {
   nome: 'Ana',
   idade: 36,
