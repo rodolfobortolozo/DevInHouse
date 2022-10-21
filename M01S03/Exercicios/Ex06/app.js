1288b3c0baee409f52eb4bb6efcc1c77a5c8891b
Rodolfo Bortolozo <rodolfobortolozo@yahoo.com.br>
2022-10-20 22:24:56 -0300

    Correção Exercicio

--- M01S03/Exercicios/Ex06/app.js
+++ M01S03/Exercicios/Ex06/app.js
@@ -1,18 +1,26 @@
+/*
+
+Um palindrome (palíndromo), segundo o dicionário Houaiss,
+"diz-se de frase ou palavra que se pode ler, indiferentemente, da esquerda para direita ou vice-versa", como por exemplo: osso, Ana, radar.
+
+Ou seja, é uma palavra que quando lida de trás pra frente tem o mesmo significado.
+
+Implemente uma função em JavaScript que receba uma string como parâmetro e retorne se determinada palavra é um palindromo ou não.
+
+*/
+
+
 const teste = "Ana";
 const teste2 = "Julia";
 
 
-const verificaPalindromo = (text) =>{
+const verificaPalindromo = (...text) =>{
 
-  let retorno="";
+  let contrario="";
 
-  for(let i= text.length - 1; i >= 0; i--){
+  contrario = text.reverse(); 
 
-    retorno += text[i];
-
-  }
-
-  if( retorno.toUpperCase() === retorno.toUpperCase() ){
+  if( contrario === text ){
   
     return true;
   
